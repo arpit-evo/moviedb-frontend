62aab15b60ab336367f874d733b5d03c56053cee
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-22 18:57:28 +0530

    feat: add new route for movie list empty page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import SignInPage from "./pages/SignInPage";
 import MainLayout from "./layout/MainLayout";
+import MovieList from "./pages/MovieList";
 
 function App() {
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<MainLayout/>}>
       <Route index element={<SignInPage/>}/>
+      <Route path="/movie-list-empty" element={<MovieList/>}/>
     </Route>
   ))
 
