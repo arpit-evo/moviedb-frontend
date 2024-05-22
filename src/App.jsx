8e87ea71976845c99ba735feacf2bf6f9656e5aa
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-22 18:38:46 +0530

    feat: declare router for routing using element

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import SignInPage from "./pages/SignInPage";
+import MainLayout from "./layout/MainLayout";
+
 function App() {
-  return <h1>hello World</h1>;
+
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<MainLayout/>}>
+      <Route index element={<SignInPage/>}/>
+    </Route>
+  ))
+
+  return (
+    <RouterProvider router={router}/>
+  );
 }
 
 export default App;
