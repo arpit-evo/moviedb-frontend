fc7ef6934552db110ac1fca5db0d138b45c80b2d
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-22 19:06:27 +0530

    fix: change styling of vector position fixed to relative

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -5,7 +5,7 @@ const MainLayout = () => {
   return (
     <div className="bg-color min-h-screen  text-white">
       <Outlet />
-      <img src="/Vectors.svg" className="w-full h-28 fixed bottom-0 mx-auto" />
+      <img src="/Vectors.svg" className="w-full h-28 relative bottom-0 mx-auto" />
     </div>
   );
 };
