9dcdff0866691f15c8ebaef3f4c4e4d36037ca36
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-23 14:42:10 +0530

    chnage element in route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
-import SignInPage from "./pages/SignInPage";
 import MainLayout from "./layout/MainLayout";
-import MovieList from "./pages/MovieList";
 import AddMoviePage from "./pages/AddMoviePage";
+import MovieListPage from "./pages/MovieListPage";
+import SignInPage from "./pages/SignInPage";
 import UpdateMoviePage from "./pages/UpdateMoviePage";
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<MainLayout/>}>
       <Route index element={<SignInPage/>}/>
-      <Route path="/movie-list" element={<MovieList/>}/>
+      <Route path="/movie-list" element={<MovieListPage/>}/>
       <Route path="/add-movie" element={<AddMoviePage/>}/>
       <Route path="/update-movie" element={<UpdateMoviePage/>}/>
     </Route>
