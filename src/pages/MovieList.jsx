68979e19aaebfba76183ed8dda09bfa2b415acf2
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-23 11:09:45 +0530

    feat: add conditional rendering based on movielist length

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const MovieList = () => {
-  return (
-    <div className='mx-auto w-fit text-center pt-[22.5%]'>
-      <h2 className='h2 mb-10'>Your movie list is empty</h2>
-      <button className='body-regular primary px-7 py-4 rounded-xl'>Add New Movie</button>
-    </div>
-  );
-}
+  const [movieList, setMovieList] = useState([]);
 
-export default MovieList
+  return (
+    <>
+      {movieList.length ? (
+        <div></div>
+      ) : (
+        <div className="mx-auto w-fit text-center pt-[22.5%] ">
+          <h2 className="h2 mb-10">Your movie list is empty</h2>
+          <Link
+            to="/add-movie"
+            className="h-14 body-regular primary  px-7 py-4 rounded-xl inline-block"
+          >
+            Add New Movie
+          </Link>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default MovieList;
