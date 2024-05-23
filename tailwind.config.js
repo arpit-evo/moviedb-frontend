65910b75d344d527bdc684c8bbd10666ea348bfe
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-23 17:08:16 +0530

    feat: add custom margin and padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        30: "7.5rem" /* 120px */,
+      },
+      margin: {
+        30: "7.5rem" /* 120px */,
+      },
+    },
   },
   plugins: [],
 };
