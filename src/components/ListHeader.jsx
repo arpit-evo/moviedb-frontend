f3786fd98b4e1e5a26da57680b002dcc7ceb98ab
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-23 17:28:23 +0530

    feat: add navigation to add movie on icon

--- src/components/ListHeader.jsx
+++ src/components/ListHeader.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 import { CgAdd } from "react-icons/cg";
 import { MdLogout } from "react-icons/md";
-import FlexContainer from './FlexContainer';
+import FlexContainer from "./FlexContainer";
+import { useNavigate } from "react-router-dom";
 
 const ListHeader = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="flex justify-between mb-[7.5rem]">
       <FlexContainer>
         <h2 className="h2">My Movies</h2>
-        <CgAdd className="text-[32px] mt-1" />
+        <CgAdd
+          className="text-[32px] mt-1 cursor-pointer"
+          onClick={() => navigate("/add-movie")}
+        />
       </FlexContainer>
       <FlexContainer>
         <p className="body-regular">Logout</p>
@@ -16,6 +22,6 @@ const ListHeader = () => {
       </FlexContainer>
     </div>
   );
-}
+};
 
-export default ListHeader
+export default ListHeader;
