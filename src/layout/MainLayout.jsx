fae639e6c7bd990ab86204151616b7707c52cb38
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-23 14:44:48 +0530

    fix: poistioning of footer

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -3,9 +3,13 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <div className="bg-color min-h-screen  text-white">
-      <Outlet />
-      <img src="/Vectors.svg" className="w-full h-28 absolute bottom-0 mx-auto" />
+    <div className="bg-color min-h-screen flex flex-col  text-white">
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <footer>
+        <img src="/Vectors.svg" className="w-full h-28 mx-auto" />
+      </footer>
     </div>
   );
 };
