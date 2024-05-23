91781b07faba70ca440d0a067a06841d9e888f40
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-23 17:09:30 +0530

    feat: create update movie page

--- src/pages/UpdateMoviePage.jsx
+++ src/pages/UpdateMoviePage.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import CancelButton from '../components/CancelButton';
+import SubmitButton from '../components/SubmitButton';
+import FileUploadContainer from '../components/FileUploadContainer';
 
 const UpdateMoviePage = () => {
   return (
-    <div>UpdateMoviePage</div>
-  )
+    <div className="p-30">
+      <div>
+        <h2 className="h2">Edit</h2>
+      </div>
+      <div className="mt-30 h-[504px] flex gap-32">
+        <FileUploadContainer />
+        <div>
+          <input
+            type="text"
+            placeholder="Title"
+            className="h-11 w-[362px] rounded-lg input-bg p-4 :focus outline-none mb-6 block body-small "
+          />
+          <input
+            type="number"
+            placeholder="Publishing year"
+            className="h-11 w-52 rounded-lg input-bg p-4 :focus outline-none mb-16 block body-small "
+            maxLength="4"
+          />
+          <div className="gap-4 flex">
+            <CancelButton />
+            <SubmitButton text='Update'/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default UpdateMoviePage
