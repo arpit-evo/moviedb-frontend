b9814b27423de7f1feec12b088eeef56d9aad320
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-25 12:35:00 +0530

    feat: display movies dynamic

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const MovieCard = () => {
+const MovieCard = ({ movie }) => {
   return (
     <div className="w-[11.25rem] card-bg sm:w-[17.625rem] sm:p-2 rounded-xl cursor-pointer">
       <div className="h-[15.375rem] sm:h-[25rem] w-full sm:mb-4">
         <img
-          src="/toy story-1.jpg"
+          src={movie.imageUrl}
           alt="movie poster"
           className="object-cover h-full w-full rounded-t-xl sm:rounded-xl"
         />
       </div>
       <div className="gap-2 p-3 sm:p-2">
-        <div className="text-br sm:text-bl">Movie 1</div>
-        <div className="text-bs">2021</div>
+        <div className="text-br sm:text-bl">{movie.title}</div>
+        <div className="text-bs">{movie.publishingYear}</div>
       </div>
     </div>
   );
-}
+};
 
-export default MovieCard
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import MovieCard from "./MovieCard";
 
-const MovieList = () => {
+const MovieList = ({ movies }) => {
   return (
     <div className="grid grid-cols-2 gap-x-5 gap-y-10 sm:gap-6 sm:grid-cols-4">
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
-      <MovieCard />
+      {movies.map((movie) => (
+        <MovieCard key={movie._id} movie={movie} />
+      ))}
     </div>
   );
 };
