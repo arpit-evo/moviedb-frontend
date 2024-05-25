d42d09d3a4c3da67d80cd4671a28599e63d10453
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-25 12:34:34 +0530

    feat: add logout function

--- src/components/ListHeader.jsx
+++ src/components/ListHeader.jsx
@@ -3,9 +3,16 @@ import { CgAdd } from "react-icons/cg";
 import { MdLogout } from "react-icons/md";
 import FlexContainer from "./FlexContainer";
 import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
 
 const ListHeader = () => {
   const navigate = useNavigate();
+
+  const handlelogout = () => {
+    Cookies.remove("accessToken")
+    Cookies.remove("refreshToken")
+    navigate("/",{replace:true})
+  }
 
   return (
     <div className="flex justify-between mb-20 sm:mb-30">
@@ -18,7 +25,7 @@ const ListHeader = () => {
       </FlexContainer>
       <FlexContainer>
         <p className="hidden sm:block sm:text-br">Logout</p>
-        <MdLogout className="text-2xl sm:text-[32px]" />
+        <MdLogout className="text-2xl sm:text-[32px]" onClick={handlelogout} />
       </FlexContainer>
     </div>
   );
