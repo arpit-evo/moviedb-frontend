bf680a255a0f79b7f28658008cc27d5fe37482d5
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-26 16:48:05 +0530

    fix: remove comment

--- src/pages/AddMoviePage.jsx
+++ src/pages/AddMoviePage.jsx
@@ -1,17 +1,7 @@
-import React, { useEffect } from "react";
-import Cookies from "js-cookie";
+import React from "react";
 import AddUpdateMovie from "../components/AddUpdateMovie";
-import { useNavigate } from "react-router-dom";
 
 const AddMoviePage = () => {
-  // const token = Cookies.get("accessToken");
-  // const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   if (!token) {
-  //     navigate("/");
-  //   }
-  // }, [token]);
 
   return <AddUpdateMovie heading={"Create a new movie"} />;
 };
