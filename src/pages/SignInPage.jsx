d5b90db5aabfdeebd75af5a1c411578b45db1052
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-28 10:05:16 +0530

    feat: change opasity of loader style

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -128,13 +128,13 @@ const SignInPage = () => {
           ) : (
             <>
               <svg
-                className="animate-spin h-5 w-5 text-white inline-block"
+                className="animate-spin h-6 w-6 text-white inline-block"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
               >
                 <circle
-                  className="opacity-25"
+                  className="opacity-55"
                   cx="12"
                   cy="12"
                   r="10"
@@ -142,7 +142,7 @@ const SignInPage = () => {
                   strokeWidth="4"
                 ></circle>
                 <path
-                  className="opacity-75"
+                  className="opacity-90"
                   fill="currentColor"
                   d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
                 ></path>
