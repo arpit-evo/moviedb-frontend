19b5e7e662f0a56fb4d3389be5ed6f25df991223
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-28 18:44:35 +0530

    change in redirectiomn

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import { useState } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 import AddMoviePage from "./pages/AddMoviePage";
@@ -6,54 +7,40 @@ import MovieListPage from "./pages/MovieListPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import SignInPage from "./pages/SignInPage";
 import UpdateMoviePage from "./pages/UpdateMoviePage";
-import Cookies from "js-cookie";
-import axios from "axios";
-import {
-  ACCESS_TOKEN_EXPIRE_TIME,
-  REFRESH_TOKEN_EXPIRE_TIME_SHORT,
-} from "./globalVar";
+import { useEffect } from "react";
 
 const App = () => {
-  const [token, setToken] = useState(Cookies.get("accessToken"));
+  const [token, setToken] = useState(Cookies.get("refreshToken"));
   const [isAuth, setIsAuth] = useState(!!token);
-  const refreshToken = Cookies.get("refreshToken");
-
-  useEffect(() => {
-    const refreshLogin = async () => {
-      if (!token && refreshToken) {
-        try {
-          const response = await axios.post(
-            `${import.meta.env.VITE_BASE_URL}/api/user/refresh-token`,
-            { refreshToken },
-            { withCredentials: true }
-          );
-          Cookies.set("accessToken", response.data.accessToken, {
-            expires: ACCESS_TOKEN_EXPIRE_TIME,
-          });
-          Cookies.set("refreshToken", response.data.newRefreshToken, {
-            expires: REFRESH_TOKEN_EXPIRE_TIME_SHORT,
-          });
-          setToken(response.data.accessToken);
-          setIsAuth(true);
-        } catch (error) {
-          console.log(error);
-          setIsAuth(false);
-        }
-      }
-    };
-
-    refreshLogin();
-  }, [token, refreshToken]);
+  const refreshToken = () => {
+    setIsAuth(Cookies.get("refreshToken"));
+  };
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route index element={<SignInPage />} />
+          <Route
+            index
+            element={
+              isAuth ? (
+                <MovieListPage />
+              ) : (
+                <SignInPage refreshAuth={refreshToken} />
+              )
+            }
+          />
           <Route
             path="/movie-list"
-            element={isAuth ? <MovieListPage /> : <SignInPage />}
+            element={
+              isAuth ? (
+                <MovieListPage />
+              ) : (
+                <SignInPage refreshAuth={refreshToken} />
+              )
+            }
           />
+          <Route path="/sign-in" element={<SignInPage />} />
           <Route
             path="/add"
             element={isAuth ? <AddMoviePage /> : <SignInPage />}
