d7ecb9a4fa14aaf0ba8fe88efed2bcd873c390f1
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-29 10:04:13 +0530

    fix: give aspect ratio to image

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -14,11 +14,11 @@ const MovieCard = ({ movie }) => {
       className="card-bg sm:p-2 rounded-xl cursor-pointer mx-auto max-w-fit "
       onClick={handleClick}
     >
-      <div className=" sm:mb-4 aspect-2/3">
+      <div className=" sm:mb-4 ">
         <img
           src={movie.imageUrl}
           alt="movie poster"
-          className="object-cover h-full w-full rounded-t-xl sm:rounded-xl object-center"
+          className="object-cover h-full w-full rounded-t-xl sm:rounded-xl object-center aspect-2/3"
         />
       </div>
       <div className="gap-2 p-3 sm:p-2">
