ab148e2dc52da3b0a105544731312ffd21af585f
arpit-evo <arpit.m@evolutioncloud.in>
2024-05-29 19:08:41 +0530

    feat: add searching and sorting

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+
+const Dropdown = ({ onselect ,search}) => {
+  const [selectedOption, setSelectedOption] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOptionClick = (event) => {
+    setSelectedOption(event.target.innerText);
+    setIsOpen(false);
+    onselect(event.target.getAttribute("data-key"));
+  };
+
+   useEffect(() => {
+     if (search) {
+       setSelectedOption("");
+       setIsOpen(false)
+     }
+   }, [search]);
+
+  return (
+    <div className="w-fit">
+      <button
+        className={`inline-flex justify-between items-center input-bg px-4 py-2 text-sm w-[10.625rem] hover:bg-card ${
+          isOpen ? "rounded-t-lg" : "rounded-lg"
+        }`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {search.length > 0 ? selectedOption  : "Sort By" }
+        {isOpen ? (
+          <IoIosArrowUp className="text-xl " />
+        ) : (
+          <IoIosArrowDown className="text-xl " />
+        )}
+      </button>
+      {isOpen && (
+        <ul className="absolute z-10 bg-card rounded-b-lg text-center w-[10.625rem]">
+          <li
+            data-key={"title"}
+            className="cursor-pointer border-y border-teal-900 px-4 py-1 hover:bg-teal-900"
+            onClick={handleOptionClick}
+          >
+            Title
+          </li>
+          <li
+            data-key={"publishingYear"}
+            className="cursor-pointer  border-b border-teal-900 px-4 py-1 hover:bg-teal-900"
+            onClick={handleOptionClick}
+          >
+            Publishibg Year
+          </li>
+          <li
+            data-key={"createdAt"}
+            className="cursor-pointer px-4 py-1 hover:bg-teal-900"
+            onClick={handleOptionClick}
+          >
+            CreatedAt
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,7 +1,9 @@
 import Cookies from "js-cookie";
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import { RiSearchLine } from "react-icons/ri";
+import { Link, useSearchParams } from "react-router-dom";
 import axiosInstance from "../apis/axiosInstance";
+import Dropdown from "../components/Dropdown";
 import ListHeader from "../components/ListHeader";
 import MovieList from "../components/MovieList";
 import Pagination from "../components/Pagination";
@@ -10,28 +12,50 @@ const MovieListPage = () => {
   const [movieList, setMovieList] = useState([]);
   const [errorMessage, setError] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
-  const [movieCount, setMovieCount] = useState();
+  const [searchCount, setSearchCount] = useState(Number);
+  const [totalCount, setTotalCount] = useState(Number);
+  const [search, setSearch] = useState("");
+  const [sortBy, setSortBy] = useState({ sort: "title", order: "asc" });
+
+  const handleSelect = (key) => {
+    setSortBy((prevSortBy) => ({
+      ...prevSortBy,
+      sort: key,
+    }));
+  };
+
+  const handleOrderBtn = (e) => {
+    setSortBy((prevSortBy) => ({
+      ...prevSortBy,
+      order: prevSortBy.order === "asc" ? "desc" : "asc",
+    }));
+  };
+
   const currentPage = Number(searchParams.get("page")) || 1;
 
-  const navigate = useNavigate();
   const token = Cookies.get("accessToken");
   const refreshToken = Cookies.get("refreshToken");
-
   useEffect(() => {
     const fetchMovies = async () => {
       try {
         const response = await axiosInstance.get(
-          `/api/movie/?page=${currentPage}`
+          `/api/movie/?page=${currentPage}&sort=${sortBy.sort},${sortBy.order}&search=${search}`
         );
         setMovieList(response.data.movies);
-        setMovieCount(response.data.movieCount);
+        setSearchCount(response.data.searchCount);
+        setTotalCount(response.data.totalCount);
       } catch (error) {
         setError(error.response?.data?.message || "An error occurred");
       }
     };
+    setTimeout(() => {
+      if (search === "") {
+        setSortBy({ sort: "title", order: "asc" });
+      }
 
-    fetchMovies();
-  }, [currentPage, token, refreshToken, navigate]);
+      fetchMovies();
+    }, 1000);
+  }, [currentPage, token, refreshToken, search, sortBy]);
 
   const handlePageChange = (newPage) => {
     setSearchParams({ page: newPage });
@@ -39,15 +63,39 @@ const MovieListPage = () => {
 
   return (
     <>
-      {movieList.length ? (
+      {totalCount > 0 ? (
         <>
           <div className="px-6 py-20 sm:p-30">
             <ListHeader />
-            <MovieList movies={movieList} />
+            <div className="mb-10 flex justify-between">
+              <div className="flex items-center input-bg rounded-lg  max-w-[22rem] focus:bg-card">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="bg-transparent h-9 w-full  rounded-lg p-2 outline-none text-bs "
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                <RiSearchLine className="text-2xl mx-2" />
+              </div>
+              <div className="flex items-center text-center gap-3">
+                <Dropdown onselect={handleSelect} search={search} />
+                <button
+                  className="input-bg px-4 py-2 text-sm rounded-lg hover:bg-card"
+                  onClick={handleOrderBtn}
+                >
+                  {sortBy.order.toUpperCase()}
+                </button>
+              </div>
+            </div>
+            {movieList.length ? (
+              <MovieList movies={movieList} />
+            ) : (
+              <div className="text-bl text-center">Movies Not Found</div>
+            )}
             {errorMessage && <div className="text-red-500">{errorMessage}</div>}
           </div>
           <Pagination
-            count={movieCount}
+            count={searchCount}
             currentPage={currentPage}
             onPageChange={handlePageChange}
           />
